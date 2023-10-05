f67815da9baf47d5c85c0ee93ae3414561d85efc
TSQ <truongsyquang1202@gmail.com>
2023-10-06 04:20:28 +0700

    Components Part 1

---
+++ my-react-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Footer.css'
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='footer-container'>
+        <h5>copyright 2022</h5>
+      </div>
+    )
+  }
+}
